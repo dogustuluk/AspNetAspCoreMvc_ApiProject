064b520b45092b004a5333f7d7bc136518031147
dogustuluk <dogus.tuluk@gmail.com>
2022-10-16 15:51:38 +0300

    repository layer and migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,12 @@
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Repository.Repositories;
+using Repository.UnitOfWorks;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);//Repository class library'nin ismini aldýk çünkü AppDbContext orada.
+    });
+});
 
 var app = builder.Build();
 
---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Configurations;
+
+namespace Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures  { get; set; } //Gerçek dünyada productFeature'ların Product üzerinden eklenmesi best practise olacaktır.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Kırmızı",
+                Height = 100,
+                Width = 200,
+                ProductId = 1
+
+
+            },
+           new ProductFeature()
+           {
+               Id = 2,
+               Color = "Mavi",
+               Height = 300,
+               Width = 500,
+               ProductId = 2
+
+
+           }
+
+
+           );
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
