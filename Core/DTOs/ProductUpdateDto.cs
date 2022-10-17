d896c19aabddd1285f08c08e3e66e9ad3668acc2
dogustuluk <dogus.tuluk@gmail.com>
2022-10-17 16:41:16 +0300

    created DTOs

---
+++ Core/DTOs/ProductUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class ProductUpdateDto
+    {//best practise açısından update için ek olarak dto açmak doğru değildir, olabildiğince ortak kullanmaya bakmalıyız. 
+        //api->dto = mvc->viewModel
+        //her bir sayfaya özgü dto-viewModel olmalıdır.
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
