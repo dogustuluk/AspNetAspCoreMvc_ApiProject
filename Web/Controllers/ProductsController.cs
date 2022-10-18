085ea9b93f90277e10e7502cd3ef0ec20ac9c7a7
dogustuluk <dogus.tuluk@gmail.com>
2022-10-18 17:49:15 +0300

    updated ProductsController and fixed bug

--- Web/Controllers/ProductsController.cs
+++ Web/Controllers/ProductsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Web.Controllers
 {
     public class ProductsController : Controller
-    {//cache datası alınacak.
+    {
         private readonly IProductService _services;
 
         public ProductsController(IProductService services)
@@ -14,10 +14,8 @@ namespace Web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //sadece bir web uygulaması ise customResponse dönmeye gerek yoktur. burada service katmanını değiştirmek istemediğimiz için yapıyoruz.
-            var CustomResponse = await _services.GetProductsWithCategory();
-            //return View(CustomResponse.Data); api
-            return View(CustomResponse); 
+            
+            return View(await _services.GetProductsWithCategory()); 
         }
     }
 }
