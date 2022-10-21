a31f412ec21bbffb180b083b00a444f1fb15f2ba
dogustuluk <dogus.tuluk@gmail.com>
2022-10-21 15:04:27 +0300

    Created CategoryApiService method

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using API.Filters;
+using AutoMapper;
+using Core.DTOs;
 using Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +10,19 @@ namespace API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories).ToList();
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
         }
 
         [HttpGet("[action]/{categoryId}")] //api/categories/GetSingleCategoryByIdWithProductsAsync/2 -->>>categoryId olur.
