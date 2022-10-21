7d5b01a160b8398334f51d9198b608ce25281063
dogustuluk <dogus.tuluk@gmail.com>
2022-10-21 14:17:47 +0300

    updated CreatedDate and UpdatedDate

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -19,6 +19,58 @@ namespace Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures  { get; set; } //Gerçek dünyada productFeature'ların Product üzerinden eklenmesi best practise olacaktır.
+        //veri tabanına yansıtılacak olan entity'lerin veri tabanına yansıtmadan önce ilgili entitylerin created ve updated tarihlerini güncelledik.
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.Entity)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if(item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;//update yapıldığı zaman createdDate'ini değiştirmiyoruz bu sayede.
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
